fc1fe1c81407b0c487f96eb5a53690605852c521
snehalkhater <snehalkhater8@gmail.com>
2026-04-03 19:23:03 +0530

    user profile page created

--- client/src/views/Profile.jsx
+++ client/src/views/Profile.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Navbar from "../components/Navbar"
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <Navbar />
+
+      <h1 className="text-3xl text-center mt-10 playpen-sans">My Profile</h1>
+      <div className="max-w-md mx-auto mt-6 bg-white p-5 rounded-lg shadow">
+        <p><strong>Name:</strong> John Doe</p>
+        <p><strong>Email:</strong> john.doe@example.com</p>
+      </div>
+    </div>
   )
 }
 
